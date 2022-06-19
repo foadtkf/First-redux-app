eda7deaa56be2dece54256e094a5b1a385ebc820
foadtkf <fakhrulif@gmail.com>
2022-06-20 04:57:05 +0600

    play redux

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useSelector, useDispatch } from "react-redux";
+import "./App.css";
+
+function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const increment = () => {
+    dispatch({ type: "INC" });
+  };
+  const decrement = () => {
+    dispatch({ type: "DEC" });
+  };
+  return (
+    <div className="App">
+      <h1>Counter: </h1>
+      <h2>{counter}</h2>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
+    </div>
+  );
+}
+
+export default App;
